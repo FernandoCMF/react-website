8a028e30f1479eacdc6d0e3b1cbc67c51fe6207e
nando <fmfernando61@gmail.com>
2021-08-23 21:52:20 -0300

    UPLOAD

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import '../../App.css';
+import React from 'react';
+import HeroSection from '../../Components/HeroSections/HeroSection';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
