4395e9bdc45a349e8227905f2cacba6f318acf5a
nando <fmfernando61@gmail.com>
2021-08-30 20:20:54 -0300

    Finished - Component Card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Pages/Home/Home';
+import Home from './Pages/Home';
+import Services from './Pages/Services';
+import Products from './Pages/Products';
+import SignUp from './Pages/SignUp';
 function App() {
   return (
     <>
@@ -9,6 +12,9 @@ function App() {
         <NavBar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
     </>
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import React from 'react';
+import HeroSection from '../Components/HeroSections/HeroSection';
+import Card from '../Components/Card/Card';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Card />
+    </>
+  );
+}
+
+export default Home;
